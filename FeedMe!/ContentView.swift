0869b5ce0da2b2570cf591c9c1440fc4fb35414f
Vivien Geschwind <vg024@hdm-stuttgart.de>
2020-04-29 16:28:33 +0200

    Initial Commit

---
+++ FeedMe!/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FeedMe!
+//
+//  Created by Vivien Geschwind on 29.04.20.
+//  Copyright © 2020 Vivien Geschwind. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
